b8724f6fafb26a3e0a126389367b265a48597165
Katherine Munro <katherine_amabel@hotmail.com>
2022-12-04 12:58:08 +0100

    Continue initial WIP

--- app.py
+++ app.py
@@ -1,10 +1,74 @@
 import streamlit as st
-import pandas as pd
-st.title("Welcome to Katherine's Website")
-name = st.text_input("Enter your name", '')
-st.write(f"Hello {name}!")
-x = st.slider("Select an integer x", 0, 10, 1)
-y = st.slider("Select an integer y", 0, 10, 1)
-df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index=["addition row"])
-st.write(df)
+import web_copy
 
+# todo poss. add animation: https://www.youtube.com/watch?v=VqgUkExPvLY and https://lottiefiles.com/
+
+st.set_page_config(layout='wide', page_title='Katherine Munro\s Personal Website', page_icon='👩‍💻')
+
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")  # todo personalise css
+
+# Banner image # TODO set size properly or else delete. Can use PIL: https://stackoverflow.com/questions/72131751/how-do-i-resize-the-image-shown-by-st-image
+# st.image(web_copy.BANNER_URL, caption='Hi. This is me, moderating TedX Innsbruck 2021')
+
+# Side menu
+section = st.sidebar.radio(label='What I do:',
+                           options=web_copy.SECTIONS.keys(), index=0)
+
+st.header('Katherine Munro')
+st.subheader('Data Scientist. Computational Linguist. Speaker, author, teacher. Geek.')
+st.write(web_copy.INTRO)
+st.write(web_copy.CTA)
+st.write('\n')
+
+# todo create function. Possibly already set the default message here
+if section == 'Data Science':
+    st.header(web_copy.SECTIONS['Data Science']['head'])
+    st.write(web_copy.SECTIONS['Data Science']['body'])
+elif section == 'Writing':
+    st.header(web_copy.SECTIONS['Writing']['head'])
+    st.write(web_copy.SECTIONS['Writing']['body'])
+elif section == 'Speaking':
+    st.header(web_copy.SECTIONS['Speaking']['head'])
+    st.write(web_copy.SECTIONS['Speaking']['body'])
+elif section == 'Teaching':
+    st.header(web_copy.SECTIONS['Teaching']['head'])
+    st.write(web_copy.SECTIONS['Teaching']['body'])
+else:
+    st.write('')
+
+# with st.container():
+#    image_col, text_col = st.columns((1, 3))
+#    with image_col:
+#        st.image(web_copy.BOOK_IMG_URL)
+#    with text_col:
+#        st.write(web_copy.BOOK_BLURB)
+
+with st.container():
+    st.write("---")
+    st.header("Let's work together!")
+    st.write("##")
+
+    # Documentation: https://formsubmit.co/
+    contact_form = f"""
+    <form action="https://formsubmit.co/katherinemunro@89gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Hi, I'd like to enquire about your {section.lower()} services." required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    l_col, m_col, r_col = st.columns((2, 1, 1))
+    with l_col:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with m_col:
+        st.empty()
+    with r_col:
+        st.image(web_copy.PROFILE_URL, width=200)
---
+++ web_copy.py
@@ -0,0 +1,23 @@
+INTRO = 'I\'m a Data Scientist and Data Science Ambassador in the e-commerce domain, conducting both research' \
+        ' and corporate training in AI, machine learning, Natural Language Processing (NLP) and data science. I\'m ' \
+        'also a speaker, writer, teacher, and passionate workaholic.'
+
+CTA = 'Use the left menu to learn more.'
+
+BANNER_URL = '/Users/katherine.munro/Downloads/TedX_1.png'
+PROFILE_URL = '/Users/katherine.munro/Downloads/smec_portrait_compressed.jpg'
+
+SECTIONS = {'Data Science': {'head': 'Data Science 👩‍💻',
+                             'body': 'I specialise in analysis, machine learning, and Natural Language Processing'},
+            'Writing':
+                {'head': 'Writing ✍️',
+                 'body': 'I edited and co-authored "The Handbook of Data Science and AI" (Hanser, 2022)'},
+            'Speaking': {'head': 'Speaking 🎤', 'body': 'I am available for lectures, event moderation, etc.'},
+            'Teaching': {'head': 'Teaching 👩‍🏫',
+                         'body': 'Check out my applied data science courses on LinkedIn Learning'}}
+
+BOOK_BLURB = 'I edited and co-wrote the textbook, \'The Handbook of Data Science and AI\''
+BOOK_IMG_URL = '/Users/katherine.munro/Documents/streamlit_website/Images/Book_Portrait.jpeg'
+
+LI_BLURB = 'View my work history and posts on data science, AI, and tech in general.'
+L_IN_ICON = '/Users/katherine.munro/Documents/streamlit_website/Images/LI-In-Bug.png'
