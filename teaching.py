311fd24ea58d37e57558d0e7a614d0555b1136d0
[KATHERINE MUNRO] <katherine_amabel@hotmail.com>
2023-01-04 13:31:28 +0100

    Experiment: add youtube video

--- teaching.py
+++ teaching.py
@@ -3,14 +3,19 @@ import streamlit as st
 
 def display_teaching():
     st.header('Teaching 👩‍🏫')
-    st.write('At just 15, I started giving guitar and piano lessons. Later, I became a telemarketing sales coach. '
-             'Later still, I taught academic English, and ESL (English as a Second Language) in Germany. '
-             'Now, I create courses on applied data science for LinkedIn Learning, and as a volunteer mentor for Female'
-             ' Coders, I give free coding lessons to women trying to get into tech.')
-    st.markdown(
-        f"""
-        You can find my LinkedIn learning courses (in German) here:
-        - [NLP Tools and Methods]({'https://www.linkedin.com/learning/natural-language-processing-nlp-mit-python-werkzeuge-und-methoden'})
-        - [Machine Learning in Practice: Marketing]({'https://www.linkedin.com/learning/machine-learning-in-der-praxis-marketing'})
-        """
-    )
+    l_col, r_col = st.columns((8, 6))
+    with l_col:
+        st.write('At just 15, I started giving guitar and piano lessons. Later, I became a telemarketing sales coach. '
+                 'Later still, I taught academic English, and ESL (English as a Second Language) in Germany. '
+                 'Now, I create courses on applied data science for LinkedIn Learning, and as a volunteer mentor for '
+                 'Female Coders, I give free coding lessons to women trying to get into tech.')
+        st.markdown(
+            f"""
+            You can find my LinkedIn learning courses (in German) here:
+            - [NLP Tools and Methods]({'https://www.linkedin.com/learning/natural-language-processing-nlp-mit-python-werkzeuge-und-methoden'})
+            - [Machine Learning in Practice: Marketing]({'https://www.linkedin.com/learning/machine-learning-in-der-praxis-marketing'})
+            """
+        )
+    with r_col:
+        st.video("https://youtu.be/i3S_KBz1nqA?t=10")
+        st.write('My lecture on predicting Customer Lifetime Value at PyData Yerevan, 2022')
